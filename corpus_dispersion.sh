ab672ef5971231b21b30510e49e396267187ca6c
Alexander Rauhut <alexanderrau@samoa.zedat.fu-berlin.de>
2021-02-04 23:22:17 +0100

    minor improvements on dispersion; added mini_collocates

--- corpus_dispersion.sh
+++ corpus_dispersion.sh
@@ -3,38 +3,42 @@ set -euo pipefail
 
 tmp="$(mktemp -d)"
 trap 'rm -rf -- "$tmp"' EXIT
-cd $tmp
+# cd $tmp
 
 corpus=$1; p_attr=$2; s_attr=$3
+n="$(cwb-lexdecode -S "$corpus" | sed -n '1s/[^0-9]//gp')"
 
-# -C option would be great, but filters most s-attributes"
+# original: # 0.5 * sum(abs(v[i]/f-s[i]))=DP
+# less heavy variation: 0.5 * sum(abs(..v[i]>1../f - s[i])) + sum(..v[i] == 0..)
+inner() { awk '{print ($1 / $2) - $3}' | sed 's/^-//g' ; }
+sum_by_key() { awk '{x[$1] += $2} END {for (i in x) print i,x[i]}' ; }
+outer() { awk '{print $1, ($2 + $3) / 2}' ; }
+
+# TODO: make an -f option to provide a preprocessed frequency list
+# get frequencies of tokens in parts: v
+# -C option would be great, but filters most s-attributes
 cwb-scan-corpus -o freq_list "$corpus" "$p_attr" "$s_attr"
 
 # sort for join; using buffer file piping directly into sort is 20% slower
-echo "preparing data" >&2
 sort -k3 freq_list > freqs_sort
 cut -f1 freqs_sort > freqs &
 cut -f2 freqs_sort > vocab &
+rm freq_list &
 
-# get parts and normalize - s = p / n; join; +1 for 0 indexing
-n="$(cwb-lexdecode -S "$corpus" | sed -n '1s/[^0-9]//gp')"
+# s = n_part / n; +1 due to 0-indexing; \ annotate list with s
 cwb-s-decode "$corpus" -S "$s_attr" \
   | awk -v n=$n '{x[$3] += $2-$1+1} END {for (i in x) print x[i] / n, i}' \
-  | sort -k2 | join -e0 -1 3 -2 2 -o 2.1 freqs_sort - > parts_perc
+  | sort -k2 | join -1 3 -2 2 -o 2.1 freqs_sort - > parts_perc
+rm freqs_sort &
 
-sum_by_key() {
-  awk '{x[$1] += $2} END {for (i in x) print i,x[i]}'
-}
+# sum parts_perc where token doesn't exist
+paste vocab parts_perc | sum_by_key | awk '{print 1 - $2}' > not_parts_perc &
 
-echo "calculating values" >&2
-paste vocab parts_perc | sum_by_key \
-  | awk '{print 1 - $2}' > not_parts_perc &
-
+# annotate corpus frequencies f
+# add values from parts without token
 cwb-lexdecode -f0 -P "$p_attr" -F vocab "$corpus" | awk '{print $1}' \
-  | paste freqs - parts_perc | awk '{print ($1 / $2) - $3}' | sed 's/^-//g' \
-  | paste vocab - | sum_by_key \
-  | paste - not_parts_perc | awk '{print $1, ($2 + $3) / 2}' \
-  | sort -n -k2
+  | paste freqs - parts_perc | inner | paste vocab - | sum_by_key \
+  | paste - not_parts_perc | outer | sort -n -k2
 
 # # split into one file per part
 # sort -k3 corp | awk '!($3 in arr) {arr[$3]=++i} {print $2, $1 > ("part_" $3)}'
---
+++ mini_collocates.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Tool to create collocation tables with counts
+# Mini version, no options, no fluff
+# Copyright (C) 2020 Alexander Rauhut
+
+data="${1:-/dev/stdin}"
+tmp="$(mktemp -d)"; trap 'rm -rf -- "$tmp"' EXIT
+[ "$data" != "$1" ] && cat $data > $tmp/table && data="$tmp/table"
+
+# infer column number from first line; parallel execution per column
+ncol=$(head -1 $data | wc -w)
+for i in $(seq $ncol); do
+  cut -f $i -d " " $data | perl -ne '
+  $count{$_}++; END { print "$count{$_} $_" for sort {
+              $count{$b} <=> $count{$a} || $a cmp $b
+            } keys %count }' > $tmp/${i}out &
+done
+wait
+
+# (don't shortcut to `-d "$delim"` or formatting breaks)
+paste $tmp/[0-9]*out | sed "s/\t\t/\t\t\t/g" | sed "s/^\t/\t\t/g"
